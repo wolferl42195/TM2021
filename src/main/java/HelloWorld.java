b1dd3373678b751a95be20a06d5b296d5d6a1304
Chris <christopher.moser@stud.fh-campuswien.ac.at>
2021-09-17 19:36:04 +0200

    Changed output message, for the weekend

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String args[]){
-        System.out.println("Hello world. - Hello Dino, we are good!");
+        System.out.println("Hello and a warm welcome to our class today. \n I hope you will have a good weekend!");
     }
 }
