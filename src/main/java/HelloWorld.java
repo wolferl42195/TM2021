d0109d790e1c49b36fdfabefc9e486bc4c6af14f
flyingcodeman <66323964+flyingcodeman@users.noreply.github.com>
2021-09-17 19:39:30 +0200

    Revert "HelloWorld fixed"

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String args[]){
-        System.out.println("Hello world - Hello Wolfgang");
+        System.out.println("Hello world - Hello Dino");
     }
 }
